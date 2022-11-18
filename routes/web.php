5a2654768f884184d07ca71a1c4a840e1b337afe
Abdelilah <lbardi.abdelilah@gmail.com>
2022-11-18 02:42:38 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('/dashboard', '/urls')->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('urls', \App\Http\Controllers\UrlsController::class)->except('show', 'edit', 'update');
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/{url:slug}', [\App\Http\Controllers\UrlsController::class, 'show']);
+
